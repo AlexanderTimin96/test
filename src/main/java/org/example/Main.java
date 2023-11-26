d1c1469046b7913912babd8b37f06f4318721428
TiminAlex1996 <timinalex.1996@gmail.com>
2023-11-26 13:11:07 +0300

    main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,5 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("main");
-        System.out.println("branch");
-        System.out.println("conflict");
     }
 }
